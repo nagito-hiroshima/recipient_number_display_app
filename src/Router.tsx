c053b5490e7c55bf4898120506a952e6a2cf5fc2
なぎと <hello@nagito.work>
2026-09-05 01:29:12 +0900

    Make interactive display root and add read-only display route

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { InputScreen } from './App';
 import { DisplayScreen } from './DisplayScreen';
+import { PublicDisplayScreen } from './PublicDisplayScreen';
 
 export const AppRouter: React.FC = () => {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<DisplayScreen />} />
+        <Route path="/display" element={<PublicDisplayScreen />} />
         <Route path="/number-input" element={<InputScreen />} />
-        <Route path="/display" element={<DisplayScreen />} />
-        <Route path="/" element={<Navigate to="/display" replace />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
   );
